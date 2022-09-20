ce96e102f4a6b9528a0177edc326a3e89ca5ef67
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-09-20 03:10:28 -0400

    Expanded Font Coverage

--- Tools/Preview.js
+++ Tools/Preview.js
@@ -13,8 +13,8 @@ canvas.text(0,10,'0123456789');
 canvas.text(0,26,'ABCDEFGHIJKL')
 canvas.text(0,38,'MNOPQRSTUVQW');
 canvas.text(0,50,'XYZ');
-canvas.text(0,62,'abcdefghujkl');
-canvas.text(0,74,'mnopqrstuvqw');
+canvas.text(0,62,'abcdefghijkl');
+canvas.text(0,74,'mnopqrstuvw');
 canvas.text(0,86,'xyz');
 
 log(canvas.render());
