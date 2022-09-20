da35277f323d271dcf974811a943e713beabd851
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-09-20 03:27:35 -0400

    Documented Font

---
+++ Documentation/Font.md
@@ -0,0 +1,22 @@
+
+# Font
+
+*Custom font used to display **Hello World**.*
+
+<br>
+
+<img
+    src = '../Assets/Font.png'
+    width = 400
+/>
+
+<br>
+
+**Location :**  [`Data/Font/`][Location]
+
+<br>
+
+
+<!----------------------------------------------------------------------------->
+
+[Location]: ../Data/Font
--- Tools/Preview.js
+++ Tools/Preview.js
@@ -9,12 +9,12 @@ const { log , clear } = console;
 clear();
 
 const canvas = new Canvas(100,100);
-canvas.text(0,10,'0123456789');
-canvas.text(0,26,'ABCDEFGHIJKL')
-canvas.text(0,38,'MNOPQRSTUVQW');
-canvas.text(0,50,'XYZ');
-canvas.text(0,62,'abcdefghijkl');
-canvas.text(0,74,'mnopqrstuvw');
-canvas.text(0,86,'xyz');
+canvas.text(10,10,'0123456789');
+canvas.text(10,26,'ABCDEFGHIJK')
+canvas.text(10,38,'LMNOPQRSTUV');
+canvas.text(10,50,'WXYZ');
+canvas.text(10,62,'abcdefghijk');
+canvas.text(10,74,'lmnopqrstuv');
+canvas.text(10,86,'wxyz');
 
 log(canvas.render());
